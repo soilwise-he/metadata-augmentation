ed637356efb8c6f721a9fe15f9582b9c4e7350e2
Aricha9801 <aricha1218@gmail.com>
2025-01-06 16:07:20 +0100

    update matchdb.py

--- keyword-matcher/match2db.py
+++ keyword-matcher/match2db.py
@@ -1,13 +1,13 @@
+# script to return the argumented result to database
+# required input: terms.csv and match.json
+
 import sys
 sys.path.append('utils')
 
-from database import dbQuery, insertMatch, deleteTable
+from database import dbQuery, insertSQL
 
 from dotenv import load_dotenv
 
-from rdflib import Graph
-
-from thefuzz import fuzz
 import json, csv
 
 def read_csv_to_dict(filename):
@@ -32,21 +32,27 @@ def get_class(t_id, mapping): # get the class by a term id
             break
     return result_c
 
-
-# load_dotenv()
-
-# # drop materialized view
-# sql = '''
-# DROP MATERIALIZED VIEW IF EXISTS public.records_terms;
-# '''
-# result = dbQuery(sql, hasoutput=False)
-
-
-# create new view
 terms = read_csv_to_dict('keyword-matcher/terms.csv')
 classes = [item['class'] for item in terms if item['class'] is not None]
-# classes_list = [c.replace(" ", "_") for c in classes]
 classes_uniq = list(set(classes))
+cols = ['identifier'] + [c.replace(" ", "_") for c in classes_uniq]
+
+# start to work on database
+load_dotenv()
+
+# first drop the m-view and the tamp table
+sql = '''
+DROP MATERIALIZED VIEW IF EXISTS records_argumented;
+DROP TABLE IF EXISTS keywords_temp;
+'''
+result = dbQuery(sql,  hasoutput=False)
+
+# create temp table
+sql = '''
+SELECT harvest.create_dynamic_table(%s, %s);
+'''
+result = dbQuery(sql, ('keywords_temp', cols), hasoutput=False)
+
 # create a mapping object
 mapping = {key: [] for key in classes_uniq}
 for t in terms:
@@ -64,16 +70,15 @@ match_withterms =[]
 keys = ['identifier'] + list(c_mapping.keys())
 default_value = None
 
-
-result = []
-
 # Group matches by record_identifier
 records = {}
 for m in match:
     record_id = m["record_identifier"]
     term_id = m["concept_identifier"]
-    records.setdefault(record_id, []).append(term_id)
+    records.setdefault(record_id, []).append(term_id) # records with unique identifier
 
+# insert target data
+res = []
 for record_id, term_ids in records.items(): # for each record (unique)
     dic = {key: None for key in keys} # initialize the div
     dic['identifier'] = record_id
@@ -87,7 +92,16 @@ for record_id, term_ids in records.items(): # for each record (unique)
                     dic[t_class] = t_label
                 else:
                     dic[t_class] = dic[t_class] + ',' + t_label
-    result.append(dic)
+    # insert row here
+    insertSQL('keywords_temp', cols, list(dic.values()) )
+    res.append(dic)
 
-print(len(result))
+# create materialized view
+## the m-view depends on the temp table, the temp_table cannot be dropped if the m-view exsists
+sql = '''
+SELECT harvest.create_dynamic_mview();
+'''
+result = dbQuery(sql, hasoutput=False)
+print('done!')
+
 
