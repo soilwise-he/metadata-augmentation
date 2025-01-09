33aa6ff620e75960d1d0a08a89dcd4bba1c54039
Aricha9801 <aricha1218@gmail.com>
2025-01-09 10:31:18 +0100

    format scripts

--- keyword-matcher/get_thesaurus.py
+++ keyword-matcher/get_thesaurus.py
@@ -108,79 +108,86 @@ def mergeLabels(dict_a, dict_b):
         merged_dict[lang] = list(dict.fromkeys(merged_dict[lang]))
     return merged_dict
 
-# Change below to a remote sparql query when the KG updated to triple store
-kg_path = "./keyword-matcher/soil_health_KG.ttl"
 
-# ! needs to be modified
-# assume only 1 exact match or close match
-# assume only 1 prefLabel
+def main():
+    # Change below to a remote sparql query when the KG updated to triple store
+    kg_path = "./keyword-matcher/soil_health_KG.ttl"
 
+    # ! needs to be modified
+    # assume only 1 exact match or close match
+    # assume only 1 prefLabel
 
-query = '''
-prefix skos: <http://www.w3.org/2004/02/skos/core#>
-SELECT DISTINCT ?concept ?label ?exact_match_uri ?close_match_uri
-WHERE {
-    ?concept a skos:Concept;
-             skos:prefLabel ?label.
-    OPTIONAL { ?concept skos:exactMatch ?exact_match_uri }
-    OPTIONAL { ?concept skos:closeMatch ?close_match_uri }
-}
-'''    
-
-kg_concs = sparqlLocal(kg_path, query, "ttl")
-# issue: a concept can have multiple labels
-langs = ["en", "fr", "de", "it", "es"]
-
-formatted_cons = []
-
-for c in kg_concs: # <= 1 exact match, <= 1 close match
-    
-    # initialize the concept dict
-    con_dict = {
-        "identifier": c.get('concept', ''),
-        "relevant_uris": [],  
-        "labels": {
-            "en": [c.get('label', '')] if c.get('label') else []
-        }
+    query = '''
+    prefix skos: <http://www.w3.org/2004/02/skos/core#>
+    SELECT DISTINCT ?concept ?label ?exact_match_uri ?close_match_uri
+    WHERE {
+        ?concept a skos:Concept;
+                skos:prefLabel ?label.
+        OPTIONAL { ?concept skos:exactMatch ?exact_match_uri }
+        OPTIONAL { ?concept skos:closeMatch ?close_match_uri }
     }
+    '''    
 
-    if c.get("exact_match_uri"):# if exact match exsists
-        uri = c["exact_match_uri"]
-        con_dict["relevant_uris"].append(uri)
-        if uri.startswith("http://aims.fao.org/aos/agrovoc"):
-            res_agro = searchAgro(uri)
-            if res_agro is not None:
-                lab_dict = processLabels(res_agro, 1, langs)
-                con_dict["labels"] = mergeLabels(con_dict["labels"], lab_dict)
+    kg_concs = sparqlLocal(kg_path, query, "ttl")
+    # issue: a concept can have multiple labels
+    langs = ["en", "fr", "de", "it", "es", "nl"]
+
+    formatted_cons = []
+
+    for c in kg_concs: # <= 1 exact match, <= 1 close match
+        
+        # initialize the concept dict
+        con_dict = {
+            "identifier": c.get('concept', ''),
+            "relevant_uris": [],  
+            "labels": {
+                "en": [c.get('label', '')] if c.get('label') else []
+            }
+        }
+
+        if c.get("exact_match_uri"):# if exact match exsists
+            uri = c["exact_match_uri"]
+            con_dict["relevant_uris"].append(uri)
+            if uri.startswith("http://aims.fao.org/aos/agrovoc"):
+                res_agro = searchAgro(uri)
+                if res_agro is not None:
+                    lab_dict = processLabels(res_agro, 1, langs)
+                    con_dict["labels"] = mergeLabels(con_dict["labels"], lab_dict)
+                    
                 
-            
-        elif uri.startswith("https://data.geoscience.earth/ncl/ISO11074"):
-            res_iso = searchIso(uri)
-            if res_iso is not None:
-                lab_dict = processLabels(res_iso, 2, langs)
-                con_dict["labels"] = mergeLabels(con_dict["labels"], lab_dict)
-        # if uri not from agro or iso, do nothing
+            elif uri.startswith("https://data.geoscience.earth/ncl/ISO11074"):
+                res_iso = searchIso(uri)
+                if res_iso is not None:
+                    lab_dict = processLabels(res_iso, 2, langs)
+                    con_dict["labels"] = mergeLabels(con_dict["labels"], lab_dict)
+            # if uri not from agro or iso, do nothing
 
-    if c.get("close_match_uri"):# if close match exsists
-        uri = c["close_match_uri"]
-        con_dict["relevant_uris"].append(uri)
-        if uri.startswith("http://aims.fao.org/aos/agrovoc"):
-            res_agro = searchAgro(uri)
-            if res_agro is not None:
-                lab_dict = processLabels(res_agro, 1, langs)
-                con_dict["labels"] = mergeLabels(con_dict["labels"], lab_dict)
-                        
-        elif uri.startwith("https://data.geoscience.earth/ncl/ISO11074"):
-            res_iso = searchIso(uri)
-            if res_iso is not None:
-                lab_dict = processLabels(res_iso, 2, langs)
-                con_dict["labels"] = mergeLabels(con_dict["labels"], lab_dict)
-    
-    formatted_cons.append(con_dict) 
+        if c.get("close_match_uri"):# if close match exsists
+            uri = c["close_match_uri"]
+            con_dict["relevant_uris"].append(uri)
+            if uri.startswith("http://aims.fao.org/aos/agrovoc"):
+                res_agro = searchAgro(uri)
+                if res_agro is not None:
+                    lab_dict = processLabels(res_agro, 1, langs)
+                    con_dict["labels"] = mergeLabels(con_dict["labels"], lab_dict)
+                            
+            elif uri.startwith("https://data.geoscience.earth/ncl/ISO11074"):
+                res_iso = searchIso(uri)
+                if res_iso is not None:
+                    lab_dict = processLabels(res_iso, 2, langs)
+                    con_dict["labels"] = mergeLabels(con_dict["labels"], lab_dict)
+        
+        formatted_cons.append(con_dict) 
 
 
-with open("./keyword-matcher/concepts.json", "w", encoding='utf-8') as json_file:
-    json.dump(formatted_cons, json_file, ensure_ascii=False, indent=2) 
+    with open("./keyword-matcher/concepts.json", "w", encoding='utf-8') as json_file:
+        json.dump(formatted_cons, json_file, ensure_ascii=False, indent=2) 
+
+    print("concepts.json updated")
+
+
+if __name__ == "__main__":
+    main()
 
 
 
