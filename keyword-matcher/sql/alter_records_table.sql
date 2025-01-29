482742c3e256574048b0da6b36c627017e8a88c8
Paul van Genuchten <genuchten@yahoo.com>
2025-01-29 14:36:54 +0100

    Create alter_records_table.sql

---
+++ keyword-matcher/sql/alter_records_table.sql
@@ -0,0 +1,15 @@
+
+ALTER TABLE records
+ADD color VARCHAR(255); 
+    soil_functions text  ,
+    soil_physical_properties text ,
+    productivity text  ,
+    soil_services text ,
+    soil_classification text ,
+    soil_processes text ,
+    soil_biological_properties text ,
+    contamination text ,
+    soil_properties text ,
+    soil_threats text ,
+    ecosystem_services text ,
+    soil_chemical_properties text
