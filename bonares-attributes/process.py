d103a07350bb3c27b2b7f63c86d175f75825c5c1
Paul van Genuchten <genuchten@yahoo.com>
2026-03-11 15:18:38 +0100

    update bonares-attributes

--- bonares-attributes/process.py
+++ bonares-attributes/process.py
@@ -20,33 +20,44 @@ with dbconn.cursor() as cur:
             WHERE
                 source='BONARES' 
             AND identifier not in (
-                select distinct record_id from attributes 
+                select distinct record_id from metadata.attributes 
             )
             AND identifier not in (
                 select record_id from metadata.augment_status where process='bonares-attributes'
-            ) limit 5""")
+            ) limit 100""")
     records = cur.fetchall()
-for rec in records:
-    rec_id, name = rec
-    # for each item, fetch attributes
+    for rec in records:
+        rec_id, name = rec
+        print(name)
+        # for each item, fetch attributes
 
-    response = requests.get(f"https://maps.bonares.de/finder/resources/dataform/data/{rec_id}/en")
-    json_data = json.loads(response.text)
+        response = requests.get(f"https://maps.bonares.de/finder/resources/dataform/data/{rec_id}/en")
+        
+        try:
+            json_data = json.loads(response.text)
+        except Exception as e:
+            print('Error parsing response:', rec_id, e)
+            cur.execute("""insert into metadata.augment_status (
+                            record_id, status, process, details, date 
+                        ) values (%s,%s,%s,%s,now())""", (
+                    rec_id,'failed','bonares-attributes', f'{rec_id} parse failed. {e}'))
+            continue
 
-    fts = {}
-    for k,v in json_data.get('bonaresInfo',{}).items():
-        if k.startswith('datamodelAttribute'):
-            tbl = v.get('tableName')
-            if tbl in [None,'']:
-                tbl = 'main'
-            if not tbl in fts:
-                fts[tbl] = []
-            fts[tbl].append(v.pop("tableName"))
+        fts = {}
+        for k,v in json_data.get('bonaresInfo',{}).items():
+            if k.startswith('datamodelAttribute'):
+                tbl = v.get('tableName')
+                if tbl in [None,'']:
+                    tbl = 'main'
+                if not tbl in fts:
+                    fts[tbl] = []
+                fts[tbl].append(v.pop("tableName"))
 
-        # insert result
-        with dbconn.cursor() as cur:
-            cur.execute("insert into metadata.augments (record_id, property, value, process, date) values (%s,%s,%s,%s,now())", (rec_id, 'spatial', str(bbox), 'spatial-locator'))
-            cur.execute("insert into metadata.augment_status (record_id, status, process, date) values (%s,%s,%s,now())", (rec_id,'success','spatial-locator'))
-        dbconn.commit()
+            # insert result
+            if len(fts.keys()) > 0:
+                with dbconn.cursor() as cur:
+                    cur.execute("insert into metadata.augments (record_id, property, value, process, date) values (%s,%s,%s,%s,now())", (rec_id, 'spatial', json.dumps(fts), 'bonares-attributes'))
+                    cur.execute("insert into metadata.augment_status (record_id, status, process, date) values (%s,%s,%s,now())", (rec_id,'success','bonares-attributes'))
+                dbconn.commit()
 
 dbconn.close()
